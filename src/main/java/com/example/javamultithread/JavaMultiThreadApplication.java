46403ab7e27ef6c47a37bb0d52dc2ad29dd78eb9
wuchang <kkang0820qus@gmail.com>
2024-09-03 22:43:01 +0900

    add: 프로젝트 세팅 및 thread 생성 예제 작성

---
+++ src/main/java/com/example/javamultithread/JavaMultiThreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamultithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMultiThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMultiThreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamultithread/JavaMultiThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamultithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultiThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
