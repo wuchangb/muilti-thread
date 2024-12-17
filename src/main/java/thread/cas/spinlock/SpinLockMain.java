ca59073360689b38ec198feed1dde285b8cc99ce
변창우 <wuchang@byeonchang-uui-Macmini.local>
2024-12-17 22:04:06 +0900

    CAS 연산 기능 추가

---
+++ src/main/java/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,24 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+
+public class SpinLockMain {
+    public static void main(String[] args) {
+        SpinLockBad spinLock = new SpinLockBad();
+        Runnable runnable = () -> {
+            try {
+                spinLock.lock();
+                //critical section
+                log("비즈니스 로직 실행");
+            } finally {
+                spinLock.unLock();
+            }
+        };
+
+        Thread t1 = new Thread(runnable, "Thread-1");
+        Thread t2 = new Thread(runnable, "Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
